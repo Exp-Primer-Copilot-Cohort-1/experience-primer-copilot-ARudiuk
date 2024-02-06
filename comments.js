c4f8cc2071cd70b938291dfa5740be069df46cd8
Alexander Rudiuk <alxanderr@gmail.com>
2024-02-06 16:50:21 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,13 @@
-// create web server
-// 1. create web server
+// Create web server
+// 1. Import express module
+const express = require('express');
+const app = express();
+
+// 2. Create a web server
+app.get('/', (req, res) => {
+    res.send('Hello, world');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
+});
